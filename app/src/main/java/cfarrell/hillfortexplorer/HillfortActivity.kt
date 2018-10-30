e25a366ae5a8208471357f08685b538a32f5c347
Conor <conorf50@gmail@.com>
2018-10-30 20:22:50 +0000

    build and running on device

--- app/src/main/java/cfarrell/hillfortexplorer/HillfortActivity.kt
+++ app/src/main/java/cfarrell/hillfortexplorer/HillfortActivity.kt
@@ -2,11 +2,14 @@ package cfarrell.hillfortexplorer
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
 
-class HillfortActivity : AppCompatActivity() {
+class HillfortActivity : AppCompatActivity(), AnkoLogger {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hillfort)
+        info("Placemark Activity started..")
     }
 }
