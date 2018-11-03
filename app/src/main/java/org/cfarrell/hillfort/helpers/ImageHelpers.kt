bea692954c05593f8f6de8f5593d79ef4e1c4972
Conor <conorf50@gmail@.com>
2018-11-03 19:51:38 +0000

    remove useless code

--- app/src/main/java/org/cfarrell/hillfort/helpers/ImageHelpers.kt
+++ app/src/main/java/org/cfarrell/hillfort/helpers/ImageHelpers.kt
@@ -19,17 +19,6 @@ fun showImagePicker(parent: Activity, id: Int) {
   parent.startActivityForResult(chooser, id)
 }
 
-//fun readImage(activity: Activity, resultCode: Int, data: Intent?): Bitmap? {
-//  var bitmap: Bitmap? = null
-//  if (resultCode == Activity.RESULT_OK && data != null && data.data != null) try {
-//    bitmap = MediaStore.Images.Media.getBitmap(activity.contentResolver, data.data)
-//  } catch (e: IOException) {
-//    e.printStackTrace()
-//  }
-//  return bitmap
-//}
-
-
 
 fun readImageFromPath(context: Context, path: ArrayList<String>): Bitmap? {
   var bitmap: Bitmap? = null
