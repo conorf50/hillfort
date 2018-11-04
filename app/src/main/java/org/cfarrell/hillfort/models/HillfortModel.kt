38cfc5038dcef27d7e4287252b7a71874f35a644
Conor <conorf50@gmail@.com>
2018-11-04 00:48:17 +0000

    add flag to model

--- app/src/main/java/org/cfarrell/hillfort/models/HillfortModel.kt
+++ app/src/main/java/org/cfarrell/hillfort/models/HillfortModel.kt
@@ -7,12 +7,11 @@ import kotlinx.android.parcel.Parcelize
 data class HillfortModel(var id: Long = 0,
                           var title: String = "",
                           var description: String = "",
-                          //var image: String = "",
-                         // change this to an arrayList of URI strings
                           var image: ArrayList<String> = ArrayList(),
                          var lat : Double = 0.0,
                           var lng: Double = 0.0,
-                          var zoom: Float = 0f) : Parcelable
+                          var zoom: Float = 0f,
+                         var visitedFlag: Boolean = false) : Parcelable
 
 @Parcelize
 data class Location(var lat: Double = 0.0,
